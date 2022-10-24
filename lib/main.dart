0555d54bc3a6303c01b2c5cd3a40a53746849a04
xaiur <davinoverclock@gmail.com>
2022-10-24 23:30:09 +0800

    TextField 01

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/main.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,30 +22,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("SnackBar"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(
-                  "Apakah anda ingin melanjutkan unduhan!",
-                ),
-                action: SnackBarAction(
-                  label: "Batal",
-                  onPressed: () {
-                    print("Eh Kamu GOBLOG!");
-                  },
-                ),
-              ),
-            );
-          },
-          child: Text("Show Snackbar"),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("TextField"),
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(30),
+          child: ListView(
+            children: [
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+            ],
+          ),
+        ));
   }
 }
