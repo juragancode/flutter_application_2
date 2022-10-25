17a3efb75c7d5110098286207168e8ec5a7a57e6
xaiur <davinoverclock@gmail.com>
2022-10-25 16:03:35 +0800

    TextField 02

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,46 +17,99 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isHidden = true;
+  TextEditingController emailC = TextEditingController();
+  TextEditingController passwordC = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("TextField"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(30),
-          child: ListView(
-            children: [
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-            ],
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("TextField"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(30),
+        children: [
+          TextField(
+            controller: emailC,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next, //next kalau sudah input text
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 20,
+              ),
+              labelText: "email",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
+              prefixIcon: Icon(Icons.email),
+            ),
           ),
-        ));
+          SizedBox(height: 10),
+          TextField(
+            controller: passwordC,
+            autocorrect: false,
+            obscureText: isHidden,
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 20,
+              ),
+              labelText: "password",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
+              prefixIcon: Icon(
+                // Icons.lock,
+                Icons.vpn_key,
+              ),
+              suffixIcon: IconButton(
+                onPressed: () {
+                  if (isHidden == true) {
+                    isHidden = false;
+                  } else {
+                    isHidden = true;
+                  }
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.remove_red_eye,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 70),
+          ElevatedButton(
+            onPressed: () {
+              print(
+                  "Login successful! email: ${emailC.text}, passwordC: ${passwordC.text}");
+            },
+            child: Text("LOGIN"),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red.shade900,
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 20,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
